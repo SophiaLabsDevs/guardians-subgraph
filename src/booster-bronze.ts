63eec7debd29b81ae91d6c1e6a9d304804d6038b
c0rdeiro <francisco.cordeiro.business@gmail.com>
2024-07-06 11:02:34 +0100

    Initial commit

---
+++ src/booster-bronze.ts
@@ -0,0 +1,151 @@
+import {
+  ApprovalForAll as ApprovalForAllEvent,
+  Paused as PausedEvent,
+  Staked as StakedEvent,
+  TransferBatch as TransferBatchEvent,
+  TransferSingle as TransferSingleEvent,
+  URI as URIEvent,
+  Unpaused as UnpausedEvent,
+  Unstaked as UnstakedEvent,
+  Upgraded as UpgradedEvent
+} from "../generated/BoosterBronze/BoosterBronze"
+import {
+  ApprovalForAll,
+  Paused,
+  Staked,
+  TransferBatch,
+  TransferSingle,
+  URI,
+  Unpaused,
+  Unstaked,
+  Upgraded
+} from "../generated/schema"
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.account = event.params.account
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePaused(event: PausedEvent): void {
+  let entity = new Paused(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.account = event.params.account
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleStaked(event: StakedEvent): void {
+  let entity = new Staked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.stakeType = event.params.stakeType
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransferBatch(event: TransferBatchEvent): void {
+  let entity = new TransferBatch(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.operator = event.params.operator
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.ids = event.params.ids
+  entity.values = event.params.values
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransferSingle(event: TransferSingleEvent): void {
+  let entity = new TransferSingle(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.operator = event.params.operator
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.BoosterBronze_id = event.params.id
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleURI(event: URIEvent): void {
+  let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  entity.value = event.params.value
+  entity.BoosterBronze_id = event.params.id
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUnpaused(event: UnpausedEvent): void {
+  let entity = new Unpaused(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.account = event.params.account
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUnstaked(event: UnstakedEvent): void {
+  let entity = new Unstaked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.stakeType = event.params.stakeType
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUpgraded(event: UpgradedEvent): void {
+  let entity = new Upgraded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.stakeType = event.params.stakeType
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ tests/booster-bronze-utils.ts
@@ -0,0 +1,197 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  ApprovalForAll,
+  Paused,
+  Staked,
+  TransferBatch,
+  TransferSingle,
+  URI,
+  Unpaused,
+  Unstaked,
+  Upgraded
+} from "../generated/BoosterBronze/BoosterBronze"
+
+export function createApprovalForAllEvent(
+  account: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createPausedEvent(account: Address): Paused {
+  let pausedEvent = changetype<Paused>(newMockEvent())
+
+  pausedEvent.parameters = new Array()
+
+  pausedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+
+  return pausedEvent
+}
+
+export function createStakedEvent(user: Address, stakeType: BigInt): Staked {
+  let stakedEvent = changetype<Staked>(newMockEvent())
+
+  stakedEvent.parameters = new Array()
+
+  stakedEvent.parameters.push(
+    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
+  )
+  stakedEvent.parameters.push(
+    new ethereum.EventParam(
+      "stakeType",
+      ethereum.Value.fromUnsignedBigInt(stakeType)
+    )
+  )
+
+  return stakedEvent
+}
+
+export function createTransferBatchEvent(
+  operator: Address,
+  from: Address,
+  to: Address,
+  ids: Array<BigInt>,
+  values: Array<BigInt>
+): TransferBatch {
+  let transferBatchEvent = changetype<TransferBatch>(newMockEvent())
+
+  transferBatchEvent.parameters = new Array()
+
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("ids", ethereum.Value.fromUnsignedBigIntArray(ids))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam(
+      "values",
+      ethereum.Value.fromUnsignedBigIntArray(values)
+    )
+  )
+
+  return transferBatchEvent
+}
+
+export function createTransferSingleEvent(
+  operator: Address,
+  from: Address,
+  to: Address,
+  id: BigInt,
+  value: BigInt
+): TransferSingle {
+  let transferSingleEvent = changetype<TransferSingle>(newMockEvent())
+
+  transferSingleEvent.parameters = new Array()
+
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferSingleEvent
+}
+
+export function createURIEvent(value: string, id: BigInt): URI {
+  let uriEvent = changetype<URI>(newMockEvent())
+
+  uriEvent.parameters = new Array()
+
+  uriEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromString(value))
+  )
+  uriEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
+  )
+
+  return uriEvent
+}
+
+export function createUnpausedEvent(account: Address): Unpaused {
+  let unpausedEvent = changetype<Unpaused>(newMockEvent())
+
+  unpausedEvent.parameters = new Array()
+
+  unpausedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+
+  return unpausedEvent
+}
+
+export function createUnstakedEvent(
+  user: Address,
+  stakeType: BigInt
+): Unstaked {
+  let unstakedEvent = changetype<Unstaked>(newMockEvent())
+
+  unstakedEvent.parameters = new Array()
+
+  unstakedEvent.parameters.push(
+    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
+  )
+  unstakedEvent.parameters.push(
+    new ethereum.EventParam(
+      "stakeType",
+      ethereum.Value.fromUnsignedBigInt(stakeType)
+    )
+  )
+
+  return unstakedEvent
+}
+
+export function createUpgradedEvent(
+  user: Address,
+  stakeType: BigInt
+): Upgraded {
+  let upgradedEvent = changetype<Upgraded>(newMockEvent())
+
+  upgradedEvent.parameters = new Array()
+
+  upgradedEvent.parameters.push(
+    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
+  )
+  upgradedEvent.parameters.push(
+    new ethereum.EventParam(
+      "stakeType",
+      ethereum.Value.fromUnsignedBigInt(stakeType)
+    )
+  )
+
+  return upgradedEvent
+}
